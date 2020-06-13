496c2194da1450687060a491775240b4b8e8efda
Go Cloud <gocloud.gcp@gmail.com>
2020-06-13 21:10:23 +1000

    firebased

--- README.md
+++ README.md
@@ -6,3 +6,6 @@ npm install express handlebars consolidate
 
 firebase serve --only functions,hosting
 
+firebase deploy
+
+ref. https://www.youtube.com/watch?v=LOeioOKUKI8
--- functions/index.js
+++ functions/index.js
@@ -3,9 +3,20 @@ const firebase = require('firebase-admin');
 const express = require('express');
 const engines = require('consolidate');
 
-// const firebaseApp = firebase.initializeApp(
-//     functions.config().firebase
-// );
+const firebaseApp = firebase.initializeApp(
+    functions.config().firebase
+);
+
+async function getFacts() {
+    const ref = firebaseApp.database().ref('facts');
+    const snap = await ref.once('value');
+    return snap.val();
+}
+
+// function getFacts() {
+//     const ref = firebaseApp.database().ref('facts');
+//     return ref.once('value').then(snap => snap.val());
+// }
 
 const app = express();
 app.engine('hbs', engines.handlebars);
@@ -14,8 +25,10 @@ app.set('view engine', 'hbs');
 
 
 app.get('/', (request, response) => {
-    response.set('Cache-Control', 'public, max-age=300, s-maxage=600');
-    response.render('index', { time: Date.now() });
+    response.set('Cache-Control', 'public, max-age=30, s-maxage=60');
+    getFacts().then(facts => {
+        response.render('index', { time: Date.now(), facts });
+    });
 });
 
 exports.app = functions.https.onRequest(app);
